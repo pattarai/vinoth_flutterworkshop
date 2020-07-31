1b421aad52f0987b0e6d5f825ebabaa7216e6657
Your Name <you@example.com>
2020-07-31 13:47:46 -0700

    initial commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,51 @@
-import 'package:flutter/cupertino.dart';
-class LoginPage extends StatefulWidget{
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewDemo extends StatefulWidget {
   @override
-  Stateful<StatefulWidget>
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _WebViewDemo();
+  }
+}
+
+class _WebViewDemo extends State<WebViewDemo> {
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          initialUrl: 'https://www.google.com',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+          onPageStarted: (String url) {
+            showAlertDialog(context);
+          },
+          onPageFinished: (String url) {
+            try {
+              Navigator.pop(context);
+            } catch (e) {
+              print("Exception");
+            }
+          },
+          gestureNavigationEnabled: true,
+        );
+      }),
+    );
+  }
+
+  showAlertDialog(var context) {
+    var alertDialog = AlertDialog(
+      title: Text("Loading..."),
+      content: LinearProgressIndicator(),
+    );
+
+    showDialog(context: context, builder: (context) => alertDialog);
+  }
 }
